1ec6982236ff8d36a5e1b1e8886895605bb744bb
liyang <liyang@wikiflyer.cn>
2022-05-15 20:35:32 +0800

    first commit

---
+++ utils/welcome.js
@@ -0,0 +1,7 @@
+// (function() {
+//   const welcome = document.createElement('script')
+//   welcome.innerHTML = `console.log('=== ' + document.title + ' ===')`
+//   document.head.appendChild(welcome)
+// })()
+
+console.info('%c=== ' + document.title + ' ===', 'color:#42b883;')
